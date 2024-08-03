3b5efce0d70dceabf369662665d075c9b803d487
youssef <youssef.labidi35@gmail.com>
2024-08-03 15:02:12 -0400

    Edit LandingPage.tsx

--- front-end/src/pages/landingPage/index.tsx
+++ front-end/src/pages/landingPage/index.tsx
@@ -1,7 +1,34 @@
 import "./style.css";
+import { useState } from "react";
 
 const LandingPage = () => {
-    return <></>;
+  const [userName, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.target.id === "username-input"
+      ? setUsername(event.target.value)
+      : setPassword(event.target.value);
+  };
+
+  return (
+      <div className="panel">
+        <input
+          value={userName}
+          onChange={handleChange}
+          id="username-input"
+          className="user-info"
+        />
+        <input
+          value={password}
+          onChange={handleChange}
+          id="password-input"
+          className="user-info"
+        />
+        <button id="login">Login</button>
+        <button id="signup">Sign Up</button>
+      </div>
+  );
 };
 
 export default LandingPage;
