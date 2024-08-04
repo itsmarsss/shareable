1ccf905f6dc77771e70d9972f1e971f7860c2ea9
William Wu <williamwu277@gmail.com>
2024-08-04 01:35:47 -0400

    Added signout

--- back-end/src/routes/profile/signout.ts
+++ back-end/src/routes/profile/signout.ts
@@ -1,5 +1,28 @@
 import { router } from "./index";
+import { mongoClient } from "../../api/mongodb";
+import { userCollection, userDatabase } from "../../dotenv";
 
 router.post("/signout", async (req, res) => {
-    res.status(200);
+    
+    const token = req.body.token;
+
+    try{
+        const db = mongoClient.db(userDatabase);
+        const collection = db.collection(userCollection);
+
+        const result = await collection.updateOne(
+            { token: token },
+            { token: "" }
+        );
+
+        res.json({
+            success: true,
+            message: "You have logged out"
+        });
+
+    }catch (error) {
+        console.error("Error signing user out:", error);
+        res.status(500).send("Internal Server Error");
+    }
+
 });
