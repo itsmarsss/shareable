12fd3355e08a12616cfbfacf2fb90b8b8a4b9eaf
William Wu <williamwu277@gmail.com>
2024-08-04 01:18:46 -0400

    Update Edit

--- back-end/src/routes/profile/edit.ts
+++ back-end/src/routes/profile/edit.ts
@@ -1,5 +1,30 @@
 import { router } from "./index";
+import { mongoClient } from "../../api/mongodb";
+import { userCollection, userDatabase } from "../../dotenv";
 
-router.post("/edit", async (req, res) => {
-    res.status(200);
+router.put("/edit", async (req, res) => {
+    /*
+    try{
+        const user = req.body.username;
+        const displayName = req.body.displayName;
+        const profileUrl = req.body.profileUrl;
+        // do hashed password afterwards
+
+        if(!(user && displayName)){ // and password
+            res.json({
+                success: false,
+                message: "Insufficent data",
+            });
+            return;
+        }    
+    }
+    
+    try{
+        const db = mongoClient.db(userDatabase);
+        const collection = db.collection(userCollection);
+
+        const existingUser = await collection.findOne({
+            
+        })
+    }*/
 });
