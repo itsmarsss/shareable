414e7a4a4b7fd04ef008dfd125953e5f6ace9300
William Wu <williamwu277@gmail.com>
2024-08-04 08:24:01 -0400

    Added AI

--- back-end/src/routes/marketplace/process-item-tag.ts
+++ back-end/src/routes/marketplace/process-item-tag.ts
@@ -1,31 +1,68 @@
 import { router } from "./index";
+const executePython = require("./pythonExecutor");
 
 router.post("/process-item-tag", async (req, res) => {
 
-    const image = req.body.image;
+    const image = req.body.fileString;
+
+    try{
+        const result = executePython(image)
+        .then((output: any) => {
+        res.json({
+            success: true,
+            result: output,
+        });
+        });
+    }catch (error){
+        console.error("Error parsing photo", error);
+        res.status(500).send("Internal Server Error");
+    } 
+});
+
+
+/*
+import { router } from "./index";
+const executePython = require("./pythonExecutor");
+
+router.post("/process-item-tag", async (req, res) => {
+
+    const image = req.body.fileString;
 
     try{
         const spawn = require("child_process").spawn;
+        //const im = JSON.parse(image);
         const pythonProcess = spawn('python3',["src/scripts/imageToText.py", image]);
 
-        let result;
+        const result = await new Promise((resolve, reject) => {
+            
+            let output: unknown;
+        
+            pythonProcess.stdout.on("data", (data: any) => {
+                output = JSON.parse(data);
+                console.log(output);
+            });
+        
+            pythonProcess.stderr.on("data", (data: any) => {
+              console.error(`[python3] Error occured: ${data}`);
+              reject(`Error occured`);
+            });
+        
+            // Handle exit
+            pythonProcess.on("exit", (code: any) => {
+              console.log(`Child process exited with code ${code}`);
+              resolve(output);
+            });
+          });
 
-        pythonProcess.stdout.on('data', (data: any) => {
-            result = JSON.parse(data);
-        });
-
-        pythonProcess.stderr.on("data", (data: any) => {
-            console.error(`[python3] Error occured: ${data}`);
-        });
-
-        console.log(result);
         res.json({
             success: true,
-            result
         })
-    } catch (error){
-        console.error("Error processing image", error);
+
+    }catch (error){
+        console.error("Error parsing video", error);
         res.status(500).send("Internal Server Error");
     }
 
 });
+
+*/
