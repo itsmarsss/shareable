0c26498c2234f9e06ada9299513722879257da5a
itsmarsss <88464166+itsmarsss@users.noreply.github.com>
2024-08-04 00:45:05 -0400

    Enable authorization for routes

--- back-end/src/routes/marketplace/index.ts
+++ back-end/src/routes/marketplace/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
+import { authorization } from "../../authorization";
 
 export const router = express.Router();
 
-// router.use(authorization);
+router.use(authorization);
 
 router.get("/", async (req, res) => {
     res.status(200).send("<html>ok</html>");
